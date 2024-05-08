09cc8c512b5efcf7c996fd24843a0eb69136213c
thaynavazzoler <thayna.vazzoler@aluno.senai.br>
2024-05-08 15:45:34 -0300

    "Código comentado"

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
+// Importação das dependências necessárias
 const express = require("express");
 const { Pool } = require("pg");
 
+// Inicialização do servidor Express
 const app = express();
 
+// Configuração do Pool de conexão com o PostgreSQL
 const pool = new Pool({
   user: "postgres",
   host: "localhost",
@@ -13,10 +16,12 @@ const pool = new Pool({
 
 app.use(express.json());
 
+// Rota inicial para teste do servidor
 app.get("/", (req, res) => {
   res.send("A rota está funcionando perfeitamente!");
 });
 
+// Rota para buscar todos os heróis
 app.get("/herois", async (req, res) => {
   try {
     const { rows } = await pool.query("SELECT * FROM herois");
@@ -30,6 +35,7 @@ app.get("/herois", async (req, res) => {
   }
 });
 
+// Rota para buscar um herói pelo ID
 app.get("/herois/:id", async (req, res) => {
   try {
     const { id } = req.params;
@@ -46,6 +52,7 @@ app.get("/herois/:id", async (req, res) => {
   }
 });
 
+// Rota para criar um novo herói
 app.post("/herois", async (req, res) => {
   try {
     const { nome, poder, nivel, pontosdevida, ataque, defesa } = req.body;
@@ -63,6 +70,7 @@ app.post("/herois", async (req, res) => {
   }
 });
 
+// Rota para atualizar um herói existente
 app.put("/herois/:id", async (req, res) => {
   try {
     const { id } = req.params;
@@ -81,6 +89,7 @@ app.put("/herois/:id", async (req, res) => {
   }
 });
 
+// Rota para deletar um herói existente
 app.delete("/herois/:id", async (req, res) => {
   try {
     const { id } = req.params;
@@ -102,8 +111,8 @@ app.listen(3000, () => {
   console.log("Servidor iniciado na porta 3000");
 });
 
-// Filtro por nome herói
 
+// Rota para buscar heróis pelo nome
 app.get("/herois/nome/:nome", async (req, res) => {
   try {
     const { nome } = req.params;
@@ -125,8 +134,7 @@ app.get("/herois/nome/:nome", async (req, res) => {
   }
 });
 
-// Filtro por poder herói
-
+// Rota para buscar heróis pelo poder
 app.get("/herois/poder/:poder", async (req, res) => {
   try {
     const { poder } = req.params;
@@ -148,28 +156,37 @@ app.get("/herois/poder/:poder", async (req, res) => {
   }
 });
 
+// Rota para simular uma batalha entre dois heróis
 app.get("/batalhar/:idHeroi1/:idHeroi2", async (req, res) => {
   try {
     const { idHeroi1, idHeroi2 } = req.params;
 
+     // Consulta para selecionar os heróis da batalha
     const query = `
       SELECT * FROM herois WHERE id = $1 OR id = $2
     `;
 
+     // Execução da consulta
     const { rows } = await pool.query(query, [idHeroi1, idHeroi2]);
 
+    
+    // Verifica se foram encontrados exatamente dois heróis
     if (rows.length !== 2) {
       return res.status(404).send("Heróis não encontrados");
     }
 
+    // Definição dos heróis da batalha
     const heroi1 = rows[0];
     const heroi2 = rows[1];
 
+     // Simulação da batalha
     while (heroi1.pontosdevida > 0 && heroi2.pontosdevida > 0) {
       heroi2.pontosdevida -= Math.max(heroi1.ataque - heroi2.defesa, 0);
       heroi1.pontosdevida -= Math.max(heroi2.ataque - heroi1.defesa, 0);
     }
 
+    
+    // Determinação do vencedor e perdedor da batalha
     let vencedor, perdedor;
     if (heroi1.pontosdevida <= 0 && heroi2.pontosdevida <= 0) {
       vencedor = "Empate";
@@ -181,10 +198,12 @@ app.get("/batalhar/:idHeroi1/:idHeroi2", async (req, res) => {
       perdedor = heroi2;
     }
 
+     // Adição do registro de histórico da batalha
     if (vencedor !== "Empate") {
       addHistorico(vencedor, perdedor);
     }
 
+     // Resposta da batalha
     res.status(200).send({
       message: "Batalha realizada com sucesso!",
       vencedor: vencedor,
@@ -195,10 +214,13 @@ app.get("/batalhar/:idHeroi1/:idHeroi2", async (req, res) => {
   }
 });
 
+// Função para adicionar um registro de histórico da batalha
 const addHistorico = async (vencedor, perdedor) => {
+   // Obtenção da data atual
   let data = new Date();
   data = data.toISOString().split("T")[0];
   try {
+     // Inserção do registro de histórico no banco de dados
     await pool.query(
       "INSERT INTO historico (vencedor, perdedor, data) VALUES ($1, $2, $3)",
       [vencedor.id, perdedor.id, data]
@@ -208,11 +230,14 @@ const addHistorico = async (vencedor, perdedor) => {
   }
 };
 
+// Rota para buscar o histórico de batalhas
 app.get("/historico", async (req, res) => {
   try {
+     // Consulta para selecionar o histórico de batalhas
     const { rows } = await pool.query(
       "SELECT h.nome AS vencedor, h2.nome AS perdedor, data FROM historico JOIN herois h ON h.id = historico.vencedor JOIN herois h2 ON h2.id = historico.perdedor;"
     );
+    // Resposta com o histórico de batalhas
     res.status(200).send({
       message: "Histórico de batalhas encontrado com sucesso!",
       historico: rows,
